2a1549c09465ba7fa6be439a852eaa98eaaa6ab3
priyanka0523 <priyankalakkireddy533@gmail.com>
2021-04-20 01:28:48 -0400

    first git commit

---
+++ JWTAuthentication_TokenBarer/Controllers/VideoGamesController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+
+namespace JWTAuthentication_TokenBarer.Controllers
+{
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    [ApiController]
+    [Route("[controller]")]
+    public class VideoGamesController : ControllerBase
+    {
+
+
+        private readonly List<VideoGames> Games = new List<VideoGames>()
+        {
+            new VideoGames { id = 03, name = "Mario", price=1000, type="Adventure"},
+            new VideoGames { id = 02, name = "Minecraft", price=1500, type="SandBox " },
+            new VideoGames { id = 04, name = "overwatch", price=2000, type="Shootimg" },
+            new VideoGames { id = 01, name = "Apex Legends", price=2500, type="Racing" },
+
+
+        };
+
+        private readonly ILogger<VideoGamesController> _logger;
+        public VideoGamesController(ILogger<VideoGamesController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        [HttpGet]
+        public IEnumerable<VideoGames> Get()
+        {
+            return Games;
+        }
+        [HttpGet("{id:int}")]
+        public VideoGames GetOne(int id)
+        {
+            return Games.SingleOrDefault(x => x.id == id);
+        }
+
+
+
+    }
+}
+
---
+++ JWTAuthentication_TokenBarer/VideoGames.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace JWTAuthentication_TokenBarer
+{
+    public class VideoGames
+    {
+        public int id { get; set; }
+
+        public string name  { get; set; }
+         public int price  { get; set; }
+        public string type { get; set; }
+        //public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        //public string Summary { get; set; }
+    }
+}
